7998ad6ed6f10b3ae5be5759b5e7d30c0a41240b
Yeabsira Tarekegn <99181424+itsyaba@users.noreply.github.com>
2024-06-08 22:16:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ app.use(cookieParser());
 const corsOptions = {
   origin: "*",
   credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Origin",
+      "X-Requested-With",
+    ],
 };
 
 app.use(cors(corsOptions));
