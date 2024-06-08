e659e2ac7e39768dccf70f0f4cc11eb5b4fac9a8
Yeabsira Tarekegn <99181424+itsyaba@users.noreply.github.com>
2024-06-08 09:59:59 +0300

    Ready for deployment

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import path from "path";
-
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
@@ -7,53 +6,54 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./utils/connectDB.js";
 import { fileURLToPath } from "url";
-const port = process.env.PORT || 3000;
-dotenv.config();
-
 import userRoutes from "./routes/userRoutes.js";
 import imageRoutes from "./routes/imageRoutes.js";
-
 import AppError from "./middleware/errorMiddleware.js";
 import globalErrorHandler from "./controllers/errorController.js";
+
+dotenv.config();
+const port = process.env.PORT || 3000;
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 connectDB();
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 const corsOptions = {
   origin: "https://gallery-glimpse-web.vercel.app",
   credentials: true,
-  methods:["GET" , "POST" , "PUT" , "DELETE","OPTIONS"] ,
-  allowedHeaders:["Content-Type", "Authorization","Access-Control-Allow-Origin","X-Requested-With"]
-}
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin", "X-Requested-With"]
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Handle preflight requests
+
+app.use(express.static(path.join(__dirname, "public")));
+
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
-  //     app.use(cors())
 }
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/images", imageRoutes);
 
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    success: false,
-    message: "Route Not Found",
-  });
   next(new AppError(`Can Not Find ${req.originalUrl} On This Server!!!`, 404));
 });
 
+
 app.use(globalErrorHandler);
 
-app.listen(port, () =>
-  console.log(`Server Running in ${process.env.NODE_ENV} Mode On Port ${port}!`)
-);
+app.listen(port, () => {
+  console.log(`Server Running in ${process.env.NODE_ENV} Mode On Port ${port}!`);
+});
