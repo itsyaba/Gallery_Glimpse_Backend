9977fb6af850f5463e4115b55e28071cfdbe74e6
Yeabsira Tarekegn <99181424+itsyaba@users.noreply.github.com>
2024-06-08 10:22:56 +0300

    fix: cors error

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 
 connectDB();
 
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -33,27 +33,23 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests
+app.options('*', cors(corsOptions)); // Preflight requests
 
 app.use(express.static(path.join(__dirname, "public")));
-
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-
 app.use("/api/users", userRoutes);
 app.use("/api/images", imageRoutes);
 
-
 app.all("*", (req, res, next) => {
-  next(new AppError(`Can Not Find ${req.originalUrl} On This Server!!!`, 404));
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
 });
-
 
 app.use(globalErrorHandler);
 
 app.listen(port, () => {
-  console.log(`Server Running in ${process.env.NODE_ENV} Mode On Port ${port}!`);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
